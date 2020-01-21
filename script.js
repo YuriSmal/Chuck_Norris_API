5603c469ff898d567ea625f96b72cdb0fd87e0ed
Yuri Smal <smalygga1488@gmail.com>
2020-01-21 15:59:12 -0500

    function parameter have been changed

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   $("button").click(function() {
-    $.get("https://api.chucknorris.io/jokes/random ", function(data) {
+    $.get("https://api.chucknorris.io/jokes/random ", function(date) {
       $("#display").html(
         "Date: " +
-          data.created_at +
+          date.created_at +
           "<br>" +
           "Icon: " +
-          data.icon_url +
+          date.icon_url +
           "<br>" +
           "Joke: " +
-          data.value
+          date.value
       );
-      console.log(data.created_at, data.icon_url, data.value);
+      console.log(date.created_at, date.icon_url, date.value);
     });
   });
 });
